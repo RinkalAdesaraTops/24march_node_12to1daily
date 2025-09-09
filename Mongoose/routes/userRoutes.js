3f22d0009658aea961242292c2b514a8e00c8b25
Rinkal <rinkaladesara.tops@gmail.com>
2025-09-09 13:00:24 +0530

    hg

--- Mongoose/routes/userRoutes.js
+++ Mongoose/routes/userRoutes.js
@@ -3,5 +3,4 @@ var router = express.Router()
 const {addRegister,login}= require('../controllers/Usercontroller')
 router.post('/register',addRegister)
 router.post('/login',login)
-
 module.exports = router
